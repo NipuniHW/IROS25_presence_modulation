b8e9134186302b4f28033012a05cfb21fde9d039
NipuniHW <hansikawijesinghe30@gmail.com>
2025-02-04 09:15:32 +1100

    Updated presence_modulation.py for Pepper's actions

---
+++ Finals/pepper_modulation.py
@@ -0,0 +1,37 @@
+from connection import Connection
+import qi
+
+# Connect Pepper robot
+pepper = Connection()
+session = pepper.connect('localhost', '36383')
+
+# Create a proxy to the AL services
+behavior_mng_service = session.service("ALBehaviorManager")
+tts = session.service("ALTextToSpeech")
+leds = session.service("ALLeds")
+
+# To update lights
+def update_lights(light):
+    if light == 0:
+        light_n = 0.1
+    else:
+        light_n = round(max(0, light/10), 1)
+        
+    # print(f"Light_n: {light, light_n}")
+    leds.setIntensity("Face/Led/Blue/Left/225Deg/Actuator/Value", light_n)
+    leds.setIntensity("Face/Led/Blue/Left/270Deg/Actuator/Value", light_n)            
+    leds.setIntensity("Face/Led/Green/Left/225Deg/Actuator/Value", light_n)
+    leds.setIntensity("Face/Led/Green/Left/270Deg/Actuator/Value", light_n)
+    leds.setIntensity("Face/Led/Red/Left/270Deg/Actuator/Value", light_n)
+    
+# To update volume
+def update_volume(volume):    
+    volume_n = round(max(0, volume/10), 1)
+    print(f"Volume_n: {volume, volume_n}")
+    tts.setVolume(volume_n)
+    tts.say("Beep boop beep boop")
+
+# To update movements
+def update_movements(movement):
+    behavior_mng_service.stopAllBehaviors()
+    behavior_mng_service.startBehavior("modulated_actions/" + str(movement)) 
