4180999e7debac00b9cc4efe4d1d9659d028f4ad
daveh <david@redirobots.com>
2025-02-17 10:25:15 +1100

    idk, maybe test, lets see

--- Finals/arg_q_learning_online.py
+++ Finals/arg_q_learning_online.py
@@ -20,15 +20,6 @@ Online Q-Learning Documentation:
 Assumptions: All MDP state transition steps occur at a rate of 3 seconds, if this needs to change we may want to reconsider some implementation details
 
 '''
-def choose_action(q_table, current_state, config):
-    # Choose an action
-    if random.uniform(0, 1) < config.exploration_rate:
-        # Explore
-        action = random.choice(list(config.actions.keys()))
-    else:
-        # Exploit
-        action = max(q_table[current_state], key=q_table[current_state].get)
-    return action
 
 def calculate_q_value(q_table, current_state, current_action, reward, config):
     # Calculate the Q-value
